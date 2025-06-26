29c19b388632d2152d098ffb455d931722c82d8c
k.anisimov <k.anisimov@epicentrk.ua>
2025-06-26 13:10:59 +0300

    FC

---
+++ dictionary/message.go
@@ -0,0 +1,9 @@
+package dictionary
+
+const PageByIdNotFound = "Page by id %s not found"
+const ErrorParsingFilter = "Error parsing %s value %s"
+const ErrorParsingRequestBody = "Error parsing request body"
+
+const SomethingWrong = "Something wrong"
+const PageDeletedSuccessful = "Page deleted successfully"
+const SaveSuccessfulMessage = "ok"
